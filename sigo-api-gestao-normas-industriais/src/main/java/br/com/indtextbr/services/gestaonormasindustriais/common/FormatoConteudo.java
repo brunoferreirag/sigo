fada188ee3fe2e9f99a749524559954eb8e52943
bags <bruno.goncalves@gft.com>
2020-12-26 21:45:07 -0300

    Novas atualizações para o gestão de normas industriais

--- sigo-api-gestao-normas-industriais/src/main/java/br/com/indtextbr/services/gestaonormasindustriais/common/FormatoConteudo.java
+++ sigo-api-gestao-normas-industriais/src/main/java/br/com/indtextbr/services/gestaonormasindustriais/common/FormatoConteudo.java
@@ -1,8 +1,6 @@
 package br.com.indtextbr.services.gestaonormasindustriais.common;
 
-import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonInclude;
-import com.fasterxml.jackson.annotation.JsonValue;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -12,21 +10,5 @@ import lombok.Getter;
 @JsonInclude(JsonInclude.Include.NON_EMPTY)
 public enum FormatoConteudo {
 
-    PDF("00"),
-    IMG("01"),
-    LINK("02");
-	@JsonValue
-	private String value;
-
-	@JsonCreator(mode = JsonCreator.Mode.DELEGATING)
-	public static FormatoConteudo fromValue(String value) {
-		if (value != null) {
-			for (FormatoConteudo e : values()) {
-				if (e.value.equalsIgnoreCase(value)) {
-					return e;
-				}
-			}
-		}
-		throw new IllegalArgumentException();
-	}
+	PDF, LINK;
 }
